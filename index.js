79c2144b233894538be2868729eff488adf3fe45
codecamp101 <phildca114@gmail.com>
2025-04-15 14:56:41 +0100

    wordsmith

--- index.js
+++ index.js
@@ -41,4 +41,35 @@ fm.addEventListener('submit',(e) => {
 //this shows/hides the panel 
 start.addEventListener('click', () => {
     pan.classList.toggle('on');
-});
+}); 
+//this formats a Unix number into a human readable date
+const dob  = new Date('05/20/2014').getTime();//Date Format in js is mm/dd/yyyy
+pan.querySelector('#dob').textContent = Intl.DateTimeFormat('en-US',{dateStyle:'full'}).format(dob);
+
+//FUNCTIONS
+//A function is a group of statements that never run unless when called
+function calculator() {
+    console.log(2 + 4);
+    console.log(10-5);
+    console.log(10/2);
+    console.log(3*3);
+    console.log(10 % 2);//modulus(short form is mod):it means the remainder of a division
+};
+calculator();
+
+function callConfetti() {
+    /*
+        const t =' ANIME';
+    const u =6;
+    console.log(t-u);//concatenaation : joins strings together
+    */
+const d= new Date('04/14/2025').getDate();
+const m= new Date('04/14/2025').getMonth();
+const today =Date.now();
+console.log(new Date(today).getDate())
+if (new Date(today).getDate() === d && new Date(today).getMonth() ===m)
+    pan.classList.add =('bgi');
+console.log(m);
+console.log(d);
+}
+callConfetti();
