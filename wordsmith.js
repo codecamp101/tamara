57243bfb2e86d0936c5bffc2158f9c1076706cf8
codecamp101 <phildca114@gmail.com>
2025-04-17 14:52:18 +0100

    ERROR FILE CHANGE

--- wordsmith.js
+++ wordsmith.js
@@ -1,5 +1,26 @@
 const hintBtn=document.querySelector('#hintBtn')
 const txt=document.querySelector('#txt')
+const tid= setTimeout(() => {
+    document.querySelector('#action').style.backgroundImage ='none'
+    clearTimeout(tid);
+})
+let score =0;
+const checkBtn=document.querySelector('#checkBtn');
+checkBtn.addEventListener('click', () => {
+    document.querySelector('#cup >i').textContent = score;
+    score+5;
+    document.querySelector('#action').style.backgroundImage =`url('error_fawzi_mourad.gif')`;
+},  2000);
+//string methods
+//.slice( start,end )//this slices a portion of a string
+//.concat(...strings) //joins all chars(character)in a string
+//.trim( ) //removes spaces at the beginning and end of a string
+//.at(...index)   //chooces the chars at the index
+//.length  //tells how many chars are in a strings
+//.toLowerCase //writes all chars in lower case
+//.toUpperCase  //writes all the chars in upper case
+//.endsWith      //returns true if the char of the string matches(ending letter)
+//.startsWith     //returns true if the char of the srings matches(starting letter)
     hintBtn.addEventListener('click', () => {
     // txt.textContent ='I CHANGED YOU!!!';
     writeTxt ();
